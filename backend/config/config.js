0c1ccc09433555c3705968d9f85b03b9501df129
Maksimod <maksumonka@gmail.com>
2025-03-15 19:22:22 +0000

    errored

--- backend/config/config.js
+++ backend/config/config.js
@@ -3,7 +3,12 @@ const config = {
   // Базовые настройки
   server: {
     port: process.env.PORT || 6005,
-    allowedOrigins: ['http://46.35.241.37:6001', 'http://localhost:6001']
+    allowedOrigins: [
+      'http://46.35.241.37:6001', 
+      'http://localhost:6001',
+      'https://iqbanana.space',
+      'http://iqbanana.space'
+    ]
   },
   
   // Версия API
